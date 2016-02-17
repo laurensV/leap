33947dae677a2929b78a8b047a9d699cdf056f6b
Laurens Verspeek <laurens_verspeek@hotmail.com>
2016-02-17 10:05:33 +0100

    rewrite plugins

--- plugins/contrib/less/less.hooks.php
+++ plugins/contrib/less/less.hooks.php
@@ -1,13 +1,13 @@
 <?php
-function less_parse_stylesheet(&$style)
+function less_parse_stylesheet(&$style, $base_path)
 {
     if (substr($style, -5) == ".less") {
-        if ($style[0] == "/") {
+        if ($style[0] == "/" || $style[0] == "\\") {
             $style = ROOT . $style;
         }
-        chdir(ROOT . "/site/");
+        chdir($base_path);
         $less_file = array($style => "/");
         $options   = array('cache_dir' => ROOT . '/site/files/css', 'compress' => true);
-        $style     = "/site/files/css/" . Less_Cache::Get($less_file, $options);
+        $style     = BASE_URL . "/site/files/css/" . Less_Cache::Get($less_file, $options);
     }
 }
---
+++ plugins/core/admin/admin_template.php
@@ -0,0 +1,3 @@
+<div id="admin">
+<?php echo $page; ?>
+</div>
