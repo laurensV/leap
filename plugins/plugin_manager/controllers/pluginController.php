754113ff4458ef7597fee0ebeb077fd77a994598
Laurens Verspeek <laurens_verspeek@hotmail.com>
2016-03-01 14:17:40 +0100

    handle enabling/disabling plugins without db

--- plugins/plugin_manager/controllers/pluginController.php
+++ plugins/plugin_manager/controllers/pluginController.php
@@ -5,17 +5,17 @@ class pluginController extends AdminController
     public function get_plugins()
     {
         $plugins = array();
-        if($this->model->has_connection()){
-            $stmt = $this->model->query("SELECT * FROM plugins");
+        if ($this->model->has_connection()) {
+            $stmt    = $this->model->query("SELECT * FROM plugins");
             $plugins = $stmt->fetchAll();
         } else {
-        	foreach ($this->plugin_manager->all_plugins as $plugin => $path) {
-        		$plugin_info = $this->plugin_manager->parse_plugin_file($path."/".$plugin.".plugin");
-	            $enabled          = $this->plugin_manager->is_enabled($plugin);
-	            $plugin_info['pid'] = $plugin;
-	            $plugin_info['status'] = $enabled;
-	            $plugins[] = $plugin_info;
-	        }
+            foreach ($this->plugin_manager->all_plugins as $plugin => $path) {
+                $plugin_info           = $this->plugin_manager->parse_plugin_file($path . "/" . $plugin . ".plugin");
+                $enabled               = $this->plugin_manager->is_enabled($plugin);
+                $plugin_info['pid']    = $plugin;
+                $plugin_info['status'] = $enabled;
+                $plugins[]             = $plugin_info;
+            }
         }
         $this->set('plugins', $plugins);
     }
@@ -23,36 +23,48 @@ class pluginController extends AdminController
     public function enable_plugin()
     {
         $pid = arg('plugin_pid');
-        if ($pid) {
-            $sql  = "UPDATE plugins SET status=1 WHERE pid= ? ";
-            // Perform Query
-            $stmt = $this->model->run($sql, [$pid]);
-            if ($stmt->rowCount()) {
-                $message = "Plugin " . $pid . " successfully enabled.";
+        if ($this->model->has_connection()) {
+            if ($pid) {
+                $sql = "UPDATE plugins SET status=1 WHERE pid= ? ";
+                // Perform Query
+                $stmt = $this->model->run($sql, [$pid]);
+                if ($stmt->rowCount()) {
+                    $message = "Plugin " . $pid . " successfully enabled.";
+                } else {
+                    $message = "Could not enable plugin " . $pid . ".<br>";
+                }
             } else {
-                $message = "Could not enable plugin " . $pid . ".<br>";
+                $message = "No plugin specified";
             }
         } else {
-            $message = "No plugin specified";
+            /* TODO: disable plugins by adding a file .disabled to the plugin folder */
+            $message = "Can't enable plugins without db, please do this manually in the code";
         }
+
         $this->set('result_message', $message);
     }
 
     public function disable_plugin()
     {
         $plugin = arg('plugin_pid');
-        if ($plugin) {
-            $sql  = "UPDATE plugins SET status=0 WHERE pid= ? ";
-            // Perform Query
-            $stmt = $this->model->run($sql, [$plugin]);
-            if ($stmt->rowCount()) {
-                $message = "Plugin " . $plugin . " successfully disabled.";
+        if ($this->model->has_connection()) {
+            if ($plugin) {
+                $sql = "UPDATE plugins SET status=0 WHERE pid= ? ";
+                // Perform Query
+                $stmt = $this->model->run($sql, [$plugin]);
+                if ($stmt->rowCount()) {
+                    $message = "Plugin " . $plugin . " successfully disabled.";
+                } else {
+                    $message = "Could not disable plugin " . $plugin . ".<br>";
+                }
             } else {
-                $message = "Could not disable plugin " . $plugin . ".<br>";
+                $message = "No plugin specified";
             }
         } else {
-            $message = "No plugin specified";
+            /* TODO: disable plugins by adding a file .disabled to the plugin folder */
+            $message = "Can't disable plugins without db, please do this manually in the code";
         }
+
         $this->set('result_message', $message);
     }
 }
