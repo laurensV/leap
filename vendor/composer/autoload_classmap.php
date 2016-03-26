8cc498f6d0d48b109278e10830acb9dbdec2a80d
Laurens Verspeek <laurens_verspeek@hotmail.com>
2016-03-26 13:34:33 +0100

    added repo for composer

--- vendor/composer/autoload_classmap.php
+++ vendor/composer/autoload_classmap.php
@@ -6,14 +6,4 @@ $vendorDir = dirname(dirname(__FILE__));
 $baseDir = dirname($vendorDir);
 
 return array(
-    'Frameworkname\\Core\\Application' => $baseDir . '/core/application.php',
-    'Frameworkname\\Core\\Controller' => $baseDir . '/core/controller.php',
-    'Frameworkname\\Hooks' => $baseDir . '/core/hooks.php',
-    'Frameworkname\\Model' => $baseDir . '/core/model.php',
-    'Frameworkname\\PluginManager' => $baseDir . '/core/pluginmanager.php',
-    'Frameworkname\\Plugins\\Admin\\AdminController' => $baseDir . '/plugins/core/admin/controllers/AdminController.php',
-    'Frameworkname\\Router' => $baseDir . '/core/router.php',
-    'Frameworkname\\SQLHandler' => $baseDir . '/core/sqlhandler.php',
-    'Frameworkname\\Template' => $baseDir . '/core/template.php',
-    'Frameworkname\\pluginController' => $baseDir . '/plugins/core/plugin_manager/controllers/pluginController.php',
 );
