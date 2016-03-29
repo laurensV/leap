fd475da490cbb46663ec7793e278d86dea66549b
Laurens Verspeek <laurens_verspeek@hotmail.com>
2016-03-29 14:31:58 +0200

    load plugins with composer

--- plugins/plugin_manager/plugin_manager.plugin.php
+++
@@ -1,7 +0,0 @@
-<?php
-namespace Leap\Hooks\Pluginmanager {
-    function hook_adminLinks(&$links)
-    {
-        $links['plugins'] = array("link" => "admin/plugins", "name" => "Plugins", "description" => "Manage all your plugins");
-    }
-}
