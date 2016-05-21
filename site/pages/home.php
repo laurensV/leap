627b8e47f096da92e428971c37c7c53436d817e5
Laurens Verspeek <laurens_verspeek@hotmail.com>
2016-05-21 18:28:43 +0200

    gitignore phpstorm

--- site/pages/home.php
+++ site/pages/home.php
@@ -1,8 +1,1 @@
-<p class="lead">This will becomes Laurens' personal site.</p>
-<?php
-
-    $domainNames = array("DOUG.NS.CLOUDFLARE.COM", "google.com");
-
-    foreach ($domainNames as $url){
-        echo gethostbyname($url); 
-    }   
+    <p class="lead">This will becomes Laurens' personal site.</p>
