592a2da2ff7ed0b0004b70f8febfa9008989d51b
Laurens Verspeek <laurens_verspeek@hotmail.com>
2017-04-13 16:04:48 +0200

    comments in default config file

--- config/config.php
+++ config/config.php
@@ -1,18 +1,26 @@
 <?php
 return [
+    // environment variable (development, production etc.)
     'environment' => 'development',
+
+    // location of file where all dependencies between classes are defined and DI Container is created
     'dic'         => 'core/dependencies.php',
+
+    // location of file where array with middleware is defined
     'middleware'  => 'app/middleware/middlewares.php',
 
+    // location of route files
     'routes'      => [
         'app/app.routes.php'
     ],
 
+    // Some paths used for global helpers
     'paths' => [
         'libraries' => 'vendor/',
         'files'     => 'files/',
     ],
 
+    // Application specific configuration
     'application' => [
         'name'  => 'Leap - PHP Framework',
         'owner' => 'Laurens Verspeek',
@@ -20,6 +28,7 @@ return [
         'url'   => 'http://laurensverspeek.nl',
     ],
 
+    // Database configuration
     'database' => [
         'type'     => 'none',
         'database' => 'leap',
@@ -27,5 +36,4 @@ return [
         'password' => '',
         'host'     => 'localhost',
     ],
-
 ];
